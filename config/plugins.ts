4ec68fad1a24be4ba1e6a3b290d063ce587fdc0b
pedro <user@gmail.com>
2024-12-22 02:20:14 -0300

    Email
    Se agrego el plugin de nodemailer para envió de correos transaccionales. Se utilizo el servicio cowmailer de pryservice.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                host: env('SMTP_HOST', 'smtp.example.com'),
+                port: env('SMTP_PORT', 7465),
+                secure: true,
+                auth: {
+                    user: env('SMTP_USERNAME'),
+                    pass: env('SMTP_PASSWORD'),
+                },
+            },
+            settings: {
+                defaultFrom: env('SMTP_USERNAME'),
+                defaultReplyTo: env('SMTP_USERNAME'),
+            },
+        },
+    },
+});
